8822dd34a774e8f42c3623715498764c15cee868
name <zhukov.vladimerovich@gmail.com>
2024-04-21 13:14:14 +0300

    Added filters in admin panels, added inlines for author and book

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -11,10 +11,14 @@ admin.site.register(Genre)
 # admin.site.register(BookInstance)
 
 # Define the admin class
+
+class BooksInline(admin.TabularInline):
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
+    inlines = [BooksInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -22,9 +26,17 @@ admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for Book using the decorator
 
+
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    list_filter = ('author', 'genre')
+    inlines = [BooksInstanceInline]
 
 
 
@@ -33,13 +45,14 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
-
+    list_display = ('book', 'due_back', 'status', 'id')
     fieldsets = (
-        (None, {
-            'fields': ('book','imprint', 'id')
-        }),
-        ('Availability', {
-            'fields': ('status', 'due_back')
-        }),
-    )
+            (None, {
+                'fields': ('book','imprint', 'id')
+            }),
+            ('Availability', {
+                'fields': ('status', 'due_back')
+            }),
+        )
+
 
