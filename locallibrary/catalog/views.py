366c1aff77f14362a1b58a261781ab9b2b21def6
name <zhukov.vladimerovich@gmail.com>
2024-05-11 12:08:48 +0300

    Added the coin of visits by user in the site

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -19,10 +19,20 @@ def index(request):
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
+
+
+    num_authors=Author.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genre':num_genre},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,
+            'num_visits':num_visits}, # num_visits appended
     )
 
 
